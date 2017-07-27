8ea2f2bc08bbf923917e2c9f7f232741ebde5de9
aradsheybak <arad.sheybak@gmail.com>
2017-07-27 09:58:05 +0430

    Add Files

---
+++ app/src/main/java/com/example/arad_sheybak/stopwatch/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.arad_sheybak.stopwatch;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private int seconds = 0;
+    private boolean running;
+    private boolean wasRunning;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if(savedInstanceState!=null){
+            seconds=savedInstanceState.getInt("seconds");
+            running=savedInstanceState.getBoolean("running");
+            wasRunning=savedInstanceState.getBoolean("wasRunning");
+        }
+        runTimer();
+    }
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        savedInstanceState.putInt("seconds",seconds);
+        savedInstanceState.putBoolean("running", running);
+        savedInstanceState.putBoolean("wasRunning", wasRunning);
+    }
+    @Override
+    protected void onStop(){
+        super.onStop();
+        wasRunning=running;
+        running=false;
+    }
+    protected void onStart(){
+        super.onStart();
+        if (wasRunning) {
+
+            running=true;
+        }
+    }
+
+    private void runTimer() {
+        final TextView textView = (TextView) findViewById(R.id.txtCounter);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds / 3600;
+                int minutes = (seconds % 3600) / 60;
+                int secs = seconds % 60;
+                String time = String.format("%d:%02d:%02d", hours, minutes, secs);
+                textView.setText(time);
+                if (running) {
+                    seconds++;
+                }
+                    handler.postDelayed(this,1000);
+            }
+        });
+    }
+
+    public void onClickStart(View view) {
+        running = true;
+
+    }
+
+    public void onClickStop(View view) {
+        running = false;
+    }
+
+    public void onClickReset(View view) {
+        running = false;
+        seconds = 0;
+
+    }
+}
